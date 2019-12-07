77a1006dccc3a43b23e4a7ea333e353075decdcc
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-12-07 14:07:39 +0000

    Moved to using lists rather than sets

--- day3.rkt
+++ day3.rkt
@@ -18,13 +18,16 @@
     (call-with-input-file filename
         (lambda (port)
             (for/list ([line (in-lines port)])
-                (for/list ([x (string-split line ",")])
-                    (parse x))))))
+                (map parse (string-split line ","))))))
 
 (define (subpath-increment subpath)
     (Location 
         (match (Subpath-direction subpath) [#\R 1] [#\L -1] [else 0])
         (match (Subpath-direction subpath) [#\U 1] [#\D -1] [else 0])))
+
+(define (Location-+ a b)
+    (Location (+ (Location-x a) (Location-x b))
+              (+ (Location-y a) (Location-y b))))
 
 (define (subpath-move subpath [origin (Location 0 0)])
     (let ([delta (subpath-increment subpath)])
@@ -34,46 +37,44 @@
 
 (define (subpath-points subpath [origin (Location 0 0)])
     (let ([delta (subpath-increment subpath)])
-    (for/set ([i (in-range 1 (+ 1 (Subpath-distance subpath)) 1)])
-        (Location
-            (+ (* i (Location-x delta)) (Location-x origin))
-            (+ (* i (Location-y delta)) (Location-y origin))))))
+    (rest
+        (for/fold ([path (list origin)])
+                  ([i (in-range (Subpath-distance subpath))])
+            (append path (list (Location-+ delta (last path))))))))
 
 (define (path-points path [origin (Location 0 0)])
-    (define-values (_ pos)
-        (for/fold ([gamma origin]
-                   [positions (set origin)])
-                  ([subpath path])
-            (values (subpath-move subpath gamma)
-                    (set-union positions (subpath-points subpath gamma)))))
-    pos)
+    (foldl
+        (lambda (left right) (append right (subpath-points left (last right))))
+        (list origin)
+        path))
 
 (define (Location-manhattan location)
     (+ (abs (Location-x location)) (abs (Location-y location))))
 
 (require rackunit)
 (check-equal? (Location 1 2) (Location 1 2) "compare structs")
+(check-equal? (Location-+ (Location -1 1) (Location 1 -1)) (Location 0 0) "+")
 (check-equal? (Location-y (Location 1 2)) 2 "access struct fields")
 (check-equal? (parse "R80") (Subpath #\R 80) "parse stuff")
 (check-equal?
     (subpath-points (Subpath #\R 2))
-    (set (Location 1 0) (Location 2 0))
+    (list (Location 1 0) (Location 2 0))
     "Can run a sub-path")
 (check-equal?
     (subpath-points (Subpath #\D 3))
-    (set (Location 0 -1) (Location 0 -2) (Location 0 -3))
+    (list (Location 0 -1) (Location 0 -2) (Location 0 -3))
     "Can run a sub-path downwards")
 (check-equal?
     (path-points (list (Subpath #\L 2)))
-    (set (Location 0 0) (Location -1 0) (Location -2 0))
+    (list (Location 0 0) (Location -1 0) (Location -2 0))
     "Single subpath")
 (check-equal?
     (path-points (list (Subpath #\L 2) (Subpath #\U 1)))
-    (set (Location 0 0) (Location -1 0) (Location -2 0) (Location -2 1))
+    (list (Location 0 0) (Location -1 0) (Location -2 0) (Location -2 1))
     "Two subpaths")
 (check-equal?
     (path-points (list (Subpath #\R 2) (Subpath #\U 1) (Subpath #\L 1)))
-    (set (Location 0 0) (Location 1 0) (Location 2 0) (Location 2 1) (Location 1 1))
+    (list (Location 0 0) (Location 1 0) (Location 2 0) (Location 2 1) (Location 1 1))
     "Three subpaths")
 (check-equal? (Location-manhattan (Location 1 -3)) 4 "Manhattan distance")
 
@@ -83,5 +84,5 @@
     set->list
     (curryr set-remove (Location 0 0))
     (curry apply set-intersect)
-    (curry map path-points)
+    (curry map (compose list->set path-points))
     parse-paths) "day3.data")
