3bba891d9523bd01c77ddc9604659b95be248627
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-12-10 12:16:13 +0000

    Reproduces day2

--- day5.rkt
+++ day5.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require racket/list)
+(require racket/string)
 
 (struct Operation (tape-inc input-inc) #:transparent)
 (struct Output Operation (value))
@@ -8,6 +9,15 @@
 (struct Exit Operation ())
 
 (struct Tape (tape head) #:transparent)
+(struct StateMachine Tape (input output operations))
+
+(define (read-data filename)
+    (call-with-input-file filename
+        (lambda (port)
+            (map string->number (string-split (read-line port) ",")))))
+
+(define (modify-data tape noun verb)
+    (append (list (first tape)) (list noun verb) (drop tape 3)))
 
 (define (Tape-code tape)
     (remainder (list-ref (Tape-tape tape) (Tape-head tape)) 100))
@@ -52,8 +62,44 @@
           [output-index (Tape-arg tape 3 #t)])
     (WriteTape 4 0 output-index (op in-0 in-1))))
 
-(define (op-multiply tape) (binop-writeout * tape))
-(define (op-sum tape) (binop-writeout + tape))
+(define (op-multiply tape [input '()])
+    (binop-writeout * tape))
+(define (op-sum tape [input '()])
+    (binop-writeout + tape))
+(define (op-exit [tape '()] [input '()])
+    (Exit 0 0))
+(define (op-read-write tape input)
+    (WriteTape 2 1 (Tape-arg 1 #t) (first input)))
+(define (op-output tape [input '()])
+    (Output 2 0 (Tape-arg 1)))
+
+(define (make-machine tape input)
+    (StateMachine
+        tape 0
+        input (list)
+        (make-hash (list
+            (cons 1 op-sum)
+            (cons 2 op-multiply)
+            (cons 3 op-read-write)
+            (cons 4 op-output)
+            (cons 99 op-exit)))))
+
+(define (StateMachine-action machine)
+    (let* ([code (Tape-code machine)]
+           [op (hash-ref (StateMachine-operations machine) code)])
+     (op machine (StateMachine-input machine))))
+
+(define (StateMachine-next machine [do-exit #f])
+    (let* ([action (StateMachine-action machine)]
+           [output (action-output action (StateMachine-output machine))]
+           [input (action-input action (StateMachine-input machine))]
+           [tape (Tape-write (Tape-advance machine action) action)]
+           [stream (Tape-tape tape)]
+           [head (Tape-head tape)]
+           [ops (StateMachine-operations machine)]
+           [result (StateMachine stream head input output ops)])
+     (if (or do-exit (Exit? action)) result (StateMachine-next result))))
+
 
 (require rackunit)
 
@@ -92,3 +138,30 @@
         (Tape-tape (Tape-write tape (op-multiply tape)))
         '(1 102 9 2 2 6 7)
         "Param mode multiply"))
+
+(let* ([tape '(1 9 10 3 2 3 11 0 99 30 40 50)]
+       [initial (make-machine tape '())]
+       [final (StateMachine-next initial)])
+ (check-equal? (Tape-tape final) '(3500 9 10 70 2 3 11 0 99 30 40 50) "day2 example"))
+
+(let* ([tape '(1 1 1 4 99 5 6 0 99)]
+       [initial (make-machine tape '())]
+       [final (StateMachine-next initial)])
+ (check-equal? (Tape-tape final) '(30 1 1 4 2 5 6 0 99) "day2 example"))
+
+(let* ([tape '(2 4 4 5 99 0)]
+       [initial (make-machine tape '())]
+       [final (StateMachine-next initial)])
+ (check-equal? (Tape-tape final) '(2 4 4 5 99 9801) "day2 example"))
+
+(let* ([data (read-data "day2.data")]
+       [1202-data (modify-data data 12 2)]
+       [initial (make-machine 1202-data '())]
+       [final (StateMachine-next initial)])
+ (check-equal? (first (Tape-tape final)) 10566835 "day2 data"))
+
+(let* ([data (read-data "day2.data")]
+       [2347-data (modify-data data 23 47)]
+       [initial (make-machine 2347-data '())]
+       [final (StateMachine-next initial)])
+ (check-equal? (first (Tape-tape final)) 19690720 "day2 data"))
