13891dfdda6d81af40e034d42a02885ede17f648
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-12-20 20:17:16 +0000

    At long last

--- day7.rkt
+++ day7.rkt
@@ -3,14 +3,16 @@
 (require racket/list)
 (require racket/string)
 (require racket/function)
+(require racket/sequence)
 
-(struct Operation (tape-inc input-inc) #:transparent)
+(struct Operation (tape-inc input) #:transparent)
 (struct Output Operation (value))
 (struct WriteTape Operation (index value))
-(struct Exit Operation ())
+(struct Exit Operation (on-exit))
 
 (struct Tape (tape head) #:transparent)
 (struct StateMachine Tape (input output operations))
+(struct Booster (machines inputs outputs continuations) #:mutable #:transparent)
 
 (define (read-data filename)
     (call-with-input-file filename
@@ -42,9 +44,7 @@
 
 (define (action-output action stream)
     (if (Output? action) (append stream (list (Output-value action))) stream))
-(define (action-input action stream)
-    (let ([n (Operation-input-inc action)])
-    (if (> n 0) (drop stream n) stream)))
+(define (action-input action stream) (Operation-input action))
 (define (Tape-advance tape action)
     (Tape (Tape-tape tape) (+ (Tape-head tape) (Operation-tape-inc action))))
 (define (Tape-write tape action)
@@ -57,53 +57,61 @@
             (Tape (append head (list value) tail) (Tape-head tape)))
         tape))
 
-(define (binop-writeout op tape)
+(define (binop-writeout op tape input)
     (let ([in-0 (Tape-arg tape 1)]
           [in-1 (Tape-arg tape 2)]
           [output-index (Tape-arg tape 3 #t)])
-    (WriteTape 4 0 output-index (op in-0 in-1))))
+    (WriteTape 4 input output-index (op in-0 in-1))))
 
 (define (op-comparison op tape [input '()])
-    (binop-writeout (lambda (a b) (if (op a b) 1 0)) tape))
+    (binop-writeout (lambda (a b) (if (op a b) 1 0)) tape input))
 
 (define (op-multiply tape [input '()])
-    (binop-writeout * tape))
+    (binop-writeout * tape input))
 (define (op-sum tape [input '()])
-    (binop-writeout + tape))
+    (binop-writeout + tape input))
 (define (op-exit [tape '()] [input '()])
-    (Exit 0 0))
+    (Exit 0 input (lambda (x) x)))
 (define (op-read-write tape input)
-    (WriteTape 2 1 (Tape-arg tape 1 #t) (first input)))
+    (let ([value (first input)]
+          [input (rest input)])
+    (WriteTape 2 input (Tape-arg tape 1 #t) value)))
 (define (op-sync-read-write tape input input-sync)
     (when (< (length input) 1)
         (set! input (call/cc input-sync)))
-    (op-read-write tape input))
+    (if (void? input) (op-exit) (op-read-write tape input)))
+(define (op-sync-output tape input output-sync)
+    (output-sync (list (Tape-arg tape 1)))
+    (Output 2 input (Tape-arg tape 1)))
 (define (op-output tape [input '()])
-    (Output 2 0 (Tape-arg tape 1)))
+    (Output 2 input (Tape-arg tape 1)))
 (define (jump-op op tape input)
     (let ([condition (Tape-arg tape 1)]
           [location (Tape-arg tape 2)])
     (if (op condition)
-        (Operation (- location (Tape-head tape)) 0)
-        (Operation 3 0))))
+        (Operation (- location (Tape-head tape)) input)
+        (Operation 3 input))))
 
-(define (make-machine tape [input '()])
+(define (make-machine tape [input '()] [ops (void)])
     (StateMachine
-        tape 0
+        (apply list tape) 0
         input (list)
-        (make-hash (list
-            (cons 1 op-sum)
-            (cons 2 op-multiply)
-            (cons 3 op-read-write)
-            (cons 4 op-output)
-            (cons 5 (curry jump-op (lambda (x) (not (= x 0)))))
-            (cons 6 (curry jump-op (curry = 0)))
-            (cons 7 (curry op-comparison <))
-            (cons 8 (curry op-comparison =))
-            (cons 99 op-exit)))))
+        (if (void? ops)
+            (make-hash (list
+                (cons 1 op-sum)
+                (cons 2 op-multiply)
+                (cons 3 op-read-write)
+                (cons 4 op-output)
+                (cons 5 (curry jump-op (lambda (x) (not (= x 0)))))
+                (cons 6 (curry jump-op (lambda (x) (= x 0))))
+                (cons 7 (curry op-comparison <))
+                (cons 8 (curry op-comparison =))
+                (cons 99 op-exit)))
+            ops)))
 
 (define (modify-machine-op machine key value)
-    (let ([ops (hash-set! (StateMachine-operations machine) key value)])
+    (let ([ops (hash-copy (StateMachine-operations machine))])
+        (hash-set! ops key value)
         (struct-copy StateMachine machine [operations ops])))
 
 (define (StateMachine-action machine)
@@ -111,34 +119,99 @@
            [op (hash-ref (StateMachine-operations machine) code)])
      (op machine (StateMachine-input machine))))
 
-(define (StateMachine-next machine [do-exit #f])
+(define (StateMachine-next machine [input (void)])
     (let* ([action (StateMachine-action machine)]
            [output (action-output action (StateMachine-output machine))]
-           [input (action-input action (StateMachine-input machine))]
+           [input (action-input action
+                        (if (void? input) (StateMachine-input machine) input))]
            [tape (Tape-write (Tape-advance machine action) action)]
            [stream (Tape-tape tape)]
            [head (Tape-head tape)]
-           [ops (StateMachine-operations machine)]
-           [result (StateMachine stream head input output ops)])
-     (if (or do-exit (Exit? action)) result (StateMachine-next result))))
+           [result (struct-copy StateMachine machine
+                        [tape #:parent Tape stream]
+                        [head #:parent Tape head]
+                        [input input]
+                        [output output])])
+     (if (Exit? action) ((Exit-on-exit action) result) (StateMachine-next result))))
 
 (define (clone-machine machine [input '()] [output '()])
     (struct-copy StateMachine machine [input input] [output output]))
 
-(define (booster machine phases [output 0])
+(define (run-booster machine phases [output 0])
     (if (= (length phases) 0)
         output
         (let* ([input (list (first phases) output)]
                [initial (clone-machine machine input)])
-            (booster
+            (run-booster
                 machine
                 (rest phases)
                 ((compose first StateMachine-output StateMachine-next) initial)))))
 
-(define (all-settings machine)
+(define (all-settings-part-1 machine)
     (for/list ([phases (in-permutations '(0 1 2 3 4))])
-        (booster machine phases)))
+        (run-booster machine phases)))
 
+(define (all-settings-part-2 tape)
+    (for/list ([phases (in-permutations '(5 6 7 8 9))])
+        (let* ([booster (make-booster tape phases)])
+        (Booster-next! booster)
+        (first (vector-ref (Booster-inputs booster) 0)))))
+
+
+(define (Booster-set-input! booster index value)
+    (vector-set! (Booster-inputs booster) index value))
+(define (Booster-set-continuation! booster index value)
+    (vector-set! (Booster-continuations booster) index value))
+(define (Booster-set-output! booster index value)
+    (vector-set! (Booster-outputs booster) index value))
+(define (Booster-length booster) (vector-length (Booster-machines booster)))
+
+(define (Booster-run-continuation! booster index)
+    (let ([input (vector-ref (Booster-inputs booster) index)]
+          [continuation (vector-ref (Booster-continuations booster) index)])
+    (Booster-set-continuation! booster index (void))
+    (Booster-set-input! booster index '())
+    (continuation input)))
+
+(define (Booster-next! booster [index 0] [continuation (void)])
+    (when (procedure? continuation)
+        (Booster-set-continuation! booster index continuation))
+    (for ([loop (in-range index (+ index (Booster-length booster)) 1)])
+        (let* ([out-index (remainder loop (Booster-length booster))]
+               [out (vector-ref (Booster-outputs booster) out-index)]
+               [in-index (remainder (+ loop 1) (Booster-length booster))]
+               [in (vector-ref (Booster-inputs booster) in-index)])
+        (when (> (length out) 0)
+            (Booster-set-input! booster in-index (append in out))
+            (Booster-set-output! booster out-index '()))))
+    (for ([cont-index (in-range (Booster-length booster))]
+          [continuation (Booster-continuations booster)]
+          [input (Booster-inputs booster)])
+        (cond
+            [(and (procedure? continuation) (> (length input) 0))
+                (Booster-run-continuation! booster cont-index)]
+            [(StateMachine? continuation)
+                (StateMachine-next continuation input)])))
+
+(define (make-booster tape phases)
+    (let* ([machines (list->vector (map (thunk* (make-machine tape)) phases))]
+           [inputs (list->vector (map (lambda (x) (list)) phases))]
+           [outputs (map list phases)]
+           [outputs (append (rest outputs) (list (list (first phases) 0)))]
+           [booster (Booster machines inputs (list->vector outputs) (void))]
+           [machines (Booster-machines booster)])
+    (for ([index (in-range (Booster-length booster))])
+       (vector-set! machines index
+           ((compose 
+               (curryr modify-machine-op 3
+                    (curryr op-sync-read-write (curry Booster-next! booster index)))
+               (curryr modify-machine-op 4
+                    (curryr op-sync-output (curry Booster-set-output! booster index)))
+               (curryr modify-machine-op 99
+                    (thunk* (Exit 0 '() (thunk* (Booster-next! booster index (void)))))))
+            (vector-ref (Booster-machines booster) index))))
+    (set-Booster-continuations! booster (Booster-machines booster))
+    booster))
 
 (require rackunit)
 
@@ -152,15 +225,11 @@
 (check-equal? (Tape-arg (Tape '(1 105 6 2 3 2) 1) 2) 6 "Get by reference")
 (check-equal? (action-output (Operation 1 0) '(1 2)) '(1 2) "Not an outputter")
 (check-equal? (action-output (Output 1 0 5) '(1 2)) '(1 2 5) "Is an outputter")
-(check-equal? (action-input (Operation 1 0) '(1 2)) '(1 2) "Not a consumer")
-(check-equal? (action-input (Operation 1 1) '(1 2)) '(2) "Consumes one item")
-(check-equal? (action-input (Operation 1 2) '(1 2)) '() "Consumes two items")
-(check-equal? (action-input (Operation 1 3) '(1 2 3 4)) '(4) "Consumes three items")
 (check-equal? (Tape-advance (Tape '(1 2 3 4) 1) (Operation 1 3))
     (Tape '(1 2 3 4) 2) "Advance tape")
 (check-equal? (Tape-write (Tape '(1 2 3 4) 1) (Operation 1 1))
     (Tape '(1 2 3 4) 1) "No write")
-(check-equal? (Tape-write (Tape '(1 2 3 4) 1) (WriteTape 1 1 2 5))
+(check-equal? (Tape-write (Tape '(1 2 3 4) 1) (WriteTape 1 '() 2 5))
     (Tape '(1 2 5 4) 1) "Do write")
 (let ([tape (Tape '(1 2 3 4 5 6 7) 0)])
     (check-equal?
@@ -247,12 +316,43 @@
 
 (let* ([program '(3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0)]
        [machine (make-machine program)])
-    (check-equal? (apply max (all-settings machine)) 43210))
+    (check-equal? (apply max (all-settings-part-1 machine)) 43210))
 
 (let* ([program '(3 23 3 24 1002 24 10 24 1002 23 -1 23 101 5 23 23 1 24 23 23 4 23 99 0 0)]
        [machine (make-machine program)])
-    (check-equal? (apply max (all-settings machine)) 54321))
+    (check-equal? (apply max (all-settings-part-1 machine)) 54321))
 
 (let* ([program (read-data "day7.data")]
        [machine (make-machine program)])
-    (check-equal? (apply max (all-settings machine)) 101490 "part 1"))
+    (check-equal? (apply max (all-settings-part-1 machine)) 101490 "part 1"))
+
+(let* ([program '(3 1 99)]
+       [syncer (lambda (continuation) (continuation '(5)))]
+       [initial (make-machine program)]
+       [machine (modify-machine-op initial 3 (curryr op-sync-read-write syncer))])
+    (check-equal? (Tape-tape (StateMachine-next machine)) '(3 5 99)))
+
+(let* ([program '(3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26 27
+                  4 27 1001 28 -1 28 1005 28 6 99 0 0 5)]
+       [phases '(9 8 7 6 5)]
+       [booster (make-booster program phases)])
+ (Booster-next! booster)
+ (check-equal? (vector-ref (Booster-inputs booster) 0) '(139629729)
+    "part 2 solution")
+ (check-equal? (apply max (all-settings-part-2 program)) 139629729 "part 2 example"))
+
+(let* ([program '(3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26
+                  1001 54 -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55
+                  2 53 55 53 4 53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10)]
+       [phases '(9 7 8 5 6)]
+       [booster (make-booster program phases)])
+ (Booster-next! booster)
+ (check-equal? (vector-ref (Booster-inputs booster) 0) '(18216) "part 2 solution")
+ (check-equal? (apply max (all-settings-part-2 program)) 18216 "part 2 example"))
+
+(let* ([program (read-data "day7.data")] 
+       [phases '(9 8 7 6 5)]
+       [booster (make-booster program phases)])
+ (Booster-next! booster)
+ (check-equal? (vector-ref (Booster-inputs booster) 0) '(36898044) "part 2 solution")
+ (check-equal? (apply max (all-settings-part-2 program)) 61019896 "part 2 example"))
